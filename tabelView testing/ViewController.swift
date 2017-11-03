a61bcfd702c87b75809fdc7915a433420ffa87b4
Elva Wang <elva316@gmail.com>
2017-11-03 08:30:59 -0700

    first commit

--- tabelView testing/ViewController.swift
+++ tabelView testing/ViewController.swift
@@ -9,17 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var tasktTextView: UITextField!
 
+    @IBAction func beastButtonPressed(_ sender: UIButton) {
+        if let x = tasktTextView.text {
+            arr.append(x)
+            tableView.reloadData()
+        }
+    }
+    var arr =  ["hi","ho", "hum"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        //   this line mean the tableView should ask the viewConroller all the questions in extension
         // Do any additional setup after loading the view, typically from a nib.
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) ->Int {
+        return arr.count
     }
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        cell.textLabel?.text = arr[indexPath.row]
+        return cell
+    }
 
 }
 
+
+
+
+
+
+
+//
